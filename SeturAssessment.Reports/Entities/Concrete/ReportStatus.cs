7555fe6c1e7808f357a7c73a7e525a4bb600b8ad
Mustafa Fındık <mustafa.findik3@gmail.com>
2021-03-12 21:30:43 +0300

    ReportService Added and Entites

---
+++ SeturAssessment.Reports/Entities/Concrete/ReportStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SeturTest.Contact.Entities.Abstract;
+
+namespace SeturAssessment.Reports.Entities.Concrete
+{
+    public class ReportStatus:IEntity
+    {
+        public int Id { get; set; }
+        public string StatusName { get; set; }
+    }
+}
---
+++ SeturAssessment.Reports/Entities/Concrete/Reports.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SeturTest.Contact.Entities.Abstract;
+
+namespace SeturAssessment.Reports.Entities.Concrete
+{
+    public class Reports : IEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime? RequestDate { get; set; }
+        public int StatusId { get; set; }
+        public ReportStatus ReportStatus { get; set; }
+
+    } 
+}
