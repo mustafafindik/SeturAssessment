69a5fc4ec3308114ad2b1b3ba378336ba6173144
Mustafa Fındık <mustafa.findik3@gmail.com>
2021-03-13 19:40:14 +0300

    ReportService add AutofacServiceProviderFactory on Program.cs

--- SeturAssessment.ReportService/Program.cs
+++ SeturAssessment.ReportService/Program.cs
@@ -1,3 +1,4 @@
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -6,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using SeturAssessment.ReportService.Utilities.DependencyResolvers.Autofac;
 
 namespace SeturAssessment.ReportService
 {
@@ -18,6 +21,8 @@ namespace SeturAssessment.ReportService
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory(builder => builder.RegisterModule(new ServiceModule())))
+
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
