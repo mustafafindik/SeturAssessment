8600b8d188e739e31038aa0b0a6d19507e3cc5a1
Mustafa Fındık <mustafa.findik3@gmail.com>
2021-03-13 08:01:42 +0300

    ContactRepository and EntityRepository Added

---
+++ SeturAssessment.ContactService/DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using SeturAssessment.ContactService.Entities.Abstract;
+
+namespace SeturAssessment.ContactService.DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new() //Sınıf , IEntity implement Eden ve newlenebilen sınıflar sadece
+    {
+        T Get(Expression<Func<T, bool>> predicate);
+        T Get(Expression<Func<T, bool>> predicate, params string[] nav);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> predicate = null);
+        IQueryable<T> GetAll(params string[] nav);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> predicate = null, params string[] nav);
+        void Add(T entity);
+        void Delete(T entity, int id);
+        void Update(T entity, int id);
+    }
+}
---
+++ SeturAssessment.ContactService/DataAccess/Concrete/EntityRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SeturAssessment.ContactService.DataAccess.Abstract;
+using SeturAssessment.ContactService.Entities.Abstract;
+
+namespace SeturAssessment.ContactService.DataAccess.Concrete
+{
+    public class EntityRepository<T, TContext> : IEntityRepository<T> where T : class, IEntity, new() where TContext : DbContext // T:Sınıf,IentityImplementEden,Newlenebilir | TContext :DbContext inherit eden
+    {
+        private readonly TContext _context;
+        public EntityRepository(TContext context)
+        {
+            _context = context;
+        }
+
+        public T Get(Expression<Func<T, bool>> predicate)
+        {
+
+            return _context.Set<T>().AsNoTracking().SingleOrDefault(predicate);
+
+        }
+
+        public T Get(Expression<Func<T, bool>> predicate, params string[] nav)
+        {
+            var query = _context.Set<T>().AsQueryable();
+            return nav.Aggregate(query, (current, n) => current.Include(n)).SingleOrDefault(predicate);
+
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> predicate = null)
+        {
+
+            return predicate == null ? _context.Set<T>().AsNoTracking() : _context.Set<T>().Where(predicate).AsNoTracking();
+
+        }
+
+        public IQueryable<T> GetAll(params string[] nav)
+        {
+
+            var query = _context.Set<T>().AsQueryable();
+            return nav.Aggregate(query, (current, n) => current.Include(n));
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> predicate = null, params string[] nav)
+        {
+
+            var query = predicate == null ? _context.Set<T>() : _context.Set<T>().Where(predicate);
+            return nav.Aggregate(query, (current, n) => current.Include(n)).AsNoTracking();
+
+        }
+
+        public void Add(T entity)
+        {
+
+            var addedEntityEntry = _context.Entry(entity);
+            addedEntityEntry.State = EntityState.Added;
+            _context.SaveChanges();
+
+        }
+
+        public void Delete(T entity, int id)
+        {
+            var existingEntity = _context.Set<T>().Find(id);
+            _context.Remove(existingEntity);
+            _context.SaveChanges();
+
+        }
+
+        public void Update(T entity, int id)
+        {
+            var existingEntity = _context.Set<T>().Find(id);
+
+            _context.Entry(existingEntity).CurrentValues.SetValues(entity);
+            _context.SaveChanges();
+
+
+        }
+    }
+}
